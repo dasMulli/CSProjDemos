198cd6f847bbcef1261f49ed80aef4fe1f618da4
dasMulli <martin.andreas.ullrich@gmail.com>
2018-10-29 17:54:17 +0100

    Initial

---
+++ 01_MultiTarget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiTargetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 05_CodeGen/CodeGenDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeGenDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{BuildVariables.Greeting} {BuildVariables.Name}!");
+        }
+    }
+}
